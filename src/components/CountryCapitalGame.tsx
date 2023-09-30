e45e98ef43ac956d9453a2437ac693d1cb34c819
Yarin96 <yarin0008@gmail.com>
2023-09-30 19:15:27 +0300

    add game logic, bg colors, types

--- src/components/CountryCapitalGame.tsx
+++ src/components/CountryCapitalGame.tsx
@@ -1,19 +1,10 @@
+import { useState } from "react";
 import "../App.css";
 
-const randomizeData = () => {
-  return Math.random() - 0.5;
-};
-
-const getCountries = (data: Record<string, string>) => {
-  return Object.keys(data);
-};
-
-const getCapitals = (data: Record<string, string>) => {
-  return Object.values(data);
-};
-
-const arrayOfOptions = (data: Record<string, string>) => {
-  return [...getCountries(data), ...getCapitals(data)];
+type Status = "DEFAULT" | "SELECTED" | "WRONG";
+type Option = {
+  value: string;
+  status: Status;
 };
 
 export default function CountryCapitalGame({
@@ -21,13 +12,86 @@ export default function CountryCapitalGame({
 }: {
   data: Record<string, string>;
 }) {
+  const randomizeData = () => {
+    return Math.random() - 0.5;
+  };
+
+  const getCountries = (data: Record<string, string>) => {
+    return Object.keys(data);
+  };
+
+  const getCapitals = (data: Record<string, string>) => {
+    return Object.values(data);
+  };
+
+  const [selected, setSelected] = useState<Option>();
+  const [options, setOptions] = useState<Option[]>(
+    [...getCountries(data), ...getCapitals(data)]
+      .sort(randomizeData)
+      .map((option) => ({
+        value: option,
+        status: "DEFAULT",
+      }))
+  );
+
+  const isGameOver = options.length === 0;
+
+  const isPartOfPair = (opt: Option, selected: Option, option: Option) => {
+    return opt.value === selected.value || opt.value === option.value;
+  };
+
+  const validationHandler = (option: Option) => {
+    if (!selected) {
+      setSelected(option);
+      setOptions(
+        options.map((opt) => ({
+          ...opt,
+          status: opt === option ? "SELECTED" : "DEFAULT",
+        }))
+      );
+    } else {
+      const capital = data[option.value];
+      const selectedCapital = data[selected.value];
+      if (selected.value === capital || selectedCapital === option.value) {
+        setOptions(
+          options.filter((opt) => !isPartOfPair(opt, option, selected))
+        );
+      } else {
+        setOptions(
+          options.map((opt) => ({
+            ...opt,
+            status: isPartOfPair(opt, option, selected) ? "WRONG" : opt.status,
+          }))
+        );
+      }
+
+      setSelected(undefined);
+    }
+  };
+
+  const statusColorClass = (option: Option) => {
+    if (option.status === "SELECTED") {
+      return "selected";
+    } else if (option.status === "WRONG") {
+      return "wrong";
+    } else return "";
+  };
+
+  if (isGameOver) {
+    return "Congratulations!";
+  }
+
   return (
     <>
-      {arrayOfOptions(data)
-        .sort(randomizeData)
-        .map((option) => (
-          <button key={option}>{option}</button>
-        ))}
+      {options.map((option) => (
+        <button
+          key={option.value}
+          className={statusColorClass(option)}
+          onClick={() => validationHandler(option)}
+        >
+          {option.value}
+        </button>
+      ))}
     </>
   );
 }
